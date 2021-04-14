37d62907cafb5e1a8440bdea8fa7d5309f0adbab
Miller Hall <hall@hey.com>
2021-04-13 21:15:49 -0400

    Add disassembly for constant opcode

--- clox/debug.c
+++ clox/debug.c
@@ -2,8 +2,10 @@
 #include <stdio.h>
 
 #include "debug.h"
+#include "value.h"
 
 static int simpleInstruction(const char*, int);
+static int constantInstruction(const char*, Chunk*, int);
 
 void disassembleChunk(Chunk* chunk, const char* name) {
     printf("== %s ==\n", name);
@@ -20,6 +22,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
     switch (instruction) {
         case OP_RETURN:
             return simpleInstruction("OP_RETURN", offset);
+        case OP_CONSTANT:
+            return constantInstruction("OP_CONSTANT", chunk, offset);
         default:
             printf("Unknown opcode %d\n", instruction);
             return offset + 1;
@@ -29,4 +33,12 @@ int disassembleInstruction(Chunk* chunk, int offset) {
 static int simpleInstruction(const char* name, int offset) {
     printf("%s\n", name);
     return offset + 1;
+}
+
+static int constantInstruction(const char* name, Chunk* chunk, int offset) {
+    uint8_t constant = chunk->code[offset + 1];
+    printf("%-16s %4d '", name, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 2;
 }
