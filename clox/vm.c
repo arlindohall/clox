e21f52a9c63ba5e2bc08a8a9d9e731f777b01999
Miller Hall <hall@hey.com>
2021-04-13 22:24:33 -0400

    First pass at interpreter for return and constant
    Everything through the start of chapter 15.1.2

--- clox/vm.c
+++ clox/vm.c
@@ -1,6 +1,10 @@
 
+#include <stdio.h>
+
 #include "common.h"
 #include "vm.h"
+
+InterpretResult run();
 
 VM vm;
 
@@ -9,4 +13,33 @@ void initVM() {
 }
 
 void freeVM() {
+}
+
+InterpretResult interpret(Chunk* chunk) {
+    vm.chunk = chunk;
+    vm.ip = vm.chunk->code;
+    return run();
+}
+
+InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+
+    for (;;) {
+        uint8_t instruction;
+        switch (instruction = READ_BYTE()) {
+            case OP_CONSTANT: {
+                Value constant = READ_CONSTANT();
+                printValue(constant);
+                printf("\n");
+                break;
+            }
+            case OP_RETURN: {
+                return INTERPRET_OK;
+            }
+        }
+    }
+
+#undef READ_CONSTANT
+#undef READ_BYTE
 }
