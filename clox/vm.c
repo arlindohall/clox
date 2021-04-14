2451a569e625e6363e58584cf83b84fa39c9a43f
Miller Hall <hall@hey.com>
2021-04-14 08:06:14 -0400

    Add binary arithmetic operators
    Done through end of chapter 15

--- clox/vm.c
+++ clox/vm.c
@@ -41,6 +41,13 @@ InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
+#define BINARY_OP(op) \
+    do { \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b); \
+    } while (false)
+
     for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
         printf("          ");
@@ -60,7 +67,11 @@ InterpretResult run() {
                 push(constant);
                 break;
             }
-            case OP_NEGATE: push(-pop()); break;
+            case OP_ADD:        BINARY_OP(+); break;
+            case OP_SUBTRACT:   BINARY_OP(-); break;
+            case OP_MULTIPLY:   BINARY_OP(*); break;
+            case OP_DIVIDE:     BINARY_OP(/); break;
+            case OP_NEGATE:     push(-pop()); break;
             case OP_RETURN: {
                 printValue(pop());
                 printf("\n");
@@ -69,6 +80,7 @@ InterpretResult run() {
         }
     }
 
+#undef BINARY_OP
 #undef READ_CONSTANT
 #undef READ_BYTE
 }
