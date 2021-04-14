9d00f794603346e75b31d1e80fc7aa73c7d61784
Miller Hall <hall@hey.com>
2021-04-14 07:47:36 -0400

    Add vm stack for storing values

--- clox/vm.c
+++ clox/vm.c
@@ -6,14 +6,29 @@
 #include "vm.h"
 
 InterpretResult run();
+static void resetStack();
 
 VM vm;
 
 void initVM() {
-
+    resetStack();
 }
 
 void freeVM() {
+}
+
+void push(Value value) {
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop() {
+    vm.stackTop--;
+    return *vm.stackTop;
+}
+
+static void resetStack() {
+    vm.stackTop = vm.stack;
 }
 
 InterpretResult interpret(Chunk* chunk) {
@@ -28,6 +43,13 @@ InterpretResult run() {
 
     for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
+        printf("          ");
+        for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+            printf("[");
+            printValue(*slot);
+            printf("]");
+        }
+        printf("\n");
         disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
 
@@ -35,11 +57,12 @@ InterpretResult run() {
         switch (instruction = READ_BYTE()) {
             case OP_CONSTANT: {
                 Value constant = READ_CONSTANT();
-                printValue(constant);
-                printf("\n");
+                push(constant);
                 break;
             }
             case OP_RETURN: {
+                printValue(pop());
+                printf("\n");
                 return INTERPRET_OK;
             }
         }
