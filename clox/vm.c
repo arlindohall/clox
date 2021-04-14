3ca75a6297967e999161f11260f5b8e9bc5fe809
Miller Hall <hall@hey.com>
2021-04-14 07:52:08 -0400

    Add negation operation

--- clox/vm.c
+++ clox/vm.c
@@ -60,6 +60,7 @@ InterpretResult run() {
                 push(constant);
                 break;
             }
+            case OP_NEGATE: push(-pop()); break;
             case OP_RETURN: {
                 printValue(pop());
                 printf("\n");
