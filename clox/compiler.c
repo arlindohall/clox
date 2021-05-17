903801e799ad5c78fd16cb68da0d500e62b7abff
Miller Hall <hall@hey.com>
2021-05-17 16:08:33 -0400

    Add expression statement compilation

--- clox/compiler.c
+++ clox/compiler.c
@@ -202,6 +202,12 @@ static void expression() {
     parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void expressionStatement() {
+    expression();
+    consume(TOKEN_SEMICOLON, "Expect ';' after expression.");
+    emitByte(OP_POP);
+}
+
 static void printStatement() {
     expression();
     consume(TOKEN_SEMICOLON, "Expect ';' after value.");
@@ -216,6 +222,8 @@ static void declaration() {
 static void statement() {
     if (match(TOKEN_PRINT)) {
         printStatement();
+    } else {
+        expressionStatement();
     }
 }
 
