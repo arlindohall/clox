d4eb8bf9bb04ebbdc3334295700784668143a9df
Miller Hall <hall@hey.com>
2021-05-27 21:09:59 -0400

    Clean up assertions failure case test

--- test/assertion.test.sh
+++ test/assertion.test.sh
@@ -5,7 +5,9 @@ set -euxo pipefail
 . test/helpers.sh
 
 function test_assertions_fail {
-    assert_fails test/failure-scripts/assert-fail.lox "Assertions work as expected return=$?"
+    assert_fails \
+        test/failure-scripts/assert-fail.lox \
+        "Assertions work as expected return=$?"
 }
 
 test_assertions_fail
