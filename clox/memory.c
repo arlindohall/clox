1c28df7873404d0c367127f940dde867baef1967
Miller Hall <hall@hey.com>
2021-07-15 11:27:51 -0400

    Finish marking all objects

--- clox/memory.c
+++ clox/memory.c
@@ -29,6 +29,8 @@ void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
 
 void markObject(Obj* object) {
     if (object == NULL) return;
+    if (object->isMarked) return;
+
 #ifdef DEBUG_LOG_GC
     printf("Mark(ptr=%p, ", object);
     printValue(OBJ_VAL(object));
@@ -54,6 +56,43 @@ void markObject(Obj* object) {
 
 void markValue(Value value) {
     if (IS_OBJ(value)) markObject(AS_OBJ(value));
+}
+
+static void markArray(ValueArray* array) {
+    for (int i = 0; i < array->count; i++) {
+        markValue(array->values[i]);
+    }
+}
+
+static void blackenObject(Obj* object) {
+#ifdef DEBUG_LOG_GC
+    printf("Blacken(ptr=%p, ", (void*)object);
+    printValue(OBJ_VAL(object));
+    printf(")\n");
+#endif
+
+    switch (object->type) {
+        case OBJ_CLOSURE: {
+            ObjClosure* closure = (ObjClosure*)object;
+            markObject((Obj*)closure->function);
+            for (int i = 0; i < closure->upvalueCount; i++) {
+                markObject((Obj*)closure->upvalues[i]);
+            }
+            break;
+        }
+        case OBJ_FUNCTION: {
+            ObjFunction* function = (ObjFunction*)object;
+            markObject((Obj*)function->name);
+            markArray(&function->chunk.constants);
+            break;
+        }
+        case OBJ_UPVALUE:
+            markValue(((ObjUpvalue*)object)->closed);
+            break;
+        case OBJ_NATIVE:
+        case OBJ_STRING:
+            break;
+    }
 }
 
 static void freeObject(Obj* object) {
@@ -85,7 +124,8 @@ static void freeObject(Obj* object) {
             FREE_ARRAY(char, string->chars, string->length + 1);
             FREE(ObjString, object);
             break;
-        case OBJ_UPVALUE:
+        }
+        case OBJ_UPVALUE: {
             FREE(ObjUpvalue, object);
             break;
         }
@@ -113,12 +153,20 @@ static void markRoots() {
     markCompilerRoots();
 }
 
+static void traceReferences() {
+    while (vm.grayCount > 0) {
+        Obj* object = vm.grayStack[--vm.grayCount];
+        blackenObject(object);
+    }
+}
+
 void collectGarbage() {
 #ifdef DEBUG_LOG_GC
     printf("-- gc begin\n");
 #endif
 
     markRoots();
+    traceReferences();
 
 #ifdef DEBUG_LOG_GC
     printf("-- gc end\n");
