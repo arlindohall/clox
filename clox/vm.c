46cfadead505f5277cc8ae3bd43d07647f9a9c50
Miller Hall <hall@hey.com>
2021-07-18 11:14:12 -0400

    Complete method compilation and runtime initialization

--- clox/vm.c
+++ clox/vm.c
@@ -184,6 +184,13 @@ static void closeUpvalues(Value* last) {
     }
 }
 
+static void defineMethod(ObjString* name) {
+    Value method = peek(0);
+    ObjClass* clss = AS_CLASS(peek(1));
+    tableSet(&clss->methods, name, method);
+    pop();
+}
+
 static bool isFalsey(Value value) {
     return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
 }
@@ -443,6 +450,8 @@ InterpretResult run() {
             case OP_CLASS:
                 push(OBJ_VAL(newClass(READ_STRING())));
                 break;
+            case OP_METHOD:
+                defineMethod(READ_STRING());
         }
     }
 
