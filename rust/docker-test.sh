1db1c8ba1d5b518161d112b44bf5088f494f0808
Miller Hall <hall@hey.com>
2021-08-28 13:47:18 -0400

    Update docker test to only map used files

--- rust/docker-test.sh
+++ rust/docker-test.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-docker run -it --rm -v $(pwd):/loxvm -w /loxvm rust:latest bash
+docker run \
+    -it \
+    --rm \
+    -v $(pwd)/src:/loxvm/src \
+    -v $(pwd)/Cargo.lock:/loxvm/Cargo.lock \
+    -v $(pwd)/Cargo.toml:/loxvm/Cargo.toml \
+    -w /loxvm \
+    rust:latest \
+    bash
